bc11a9b2955388afff5a6057573c7bd7eed0a858
kelick <kelick@local.market85.com>
2020-07-16 17:57:56 +0900

    test

--- src/main/java/com/example/demo/Demo6Application.java
+++ src/main/java/com/example/demo/Demo6Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Demo6Application {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(Demo6Application.class, args);
 	}
 
